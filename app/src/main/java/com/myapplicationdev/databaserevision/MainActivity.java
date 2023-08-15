848c12cc19b80a98809843d92c29e853ed02c24b
22024713 <22024713@myrp.edu.sg>
2023-08-15 11:57:24 +0800

    updated main activity java

--- app/src/main/java/com/myapplicationdev/databaserevision/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/databaserevision/MainActivity.java
@@ -2,6 +2,7 @@ package com.myapplicationdev.databaserevision;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,15 +23,16 @@ public class MainActivity extends AppCompatActivity {
         btnInsertRecord.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
-
+                Intent intent = new Intent(MainActivity.this, InsertActivity.class);
+                startActivity(intent);
             }
         });
 
         btnRetrieveRecords.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, RetrieveActivityListView.class);
+                startActivity(intent);
             }
         });
 
